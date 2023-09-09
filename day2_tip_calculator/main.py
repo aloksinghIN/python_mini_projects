a367a5ddfc425f3352d0899c0a641f26b44ac59d
alok singh <er.aloksingh01@gmail.com>
2023-09-10 03:30:09 +0530

    tip calculator

--- day2_tip_calculator/main.py
+++ day2_tip_calculator/main.py
@@ -5,4 +5,5 @@ people_count = int(input("How many people to split the bill?\n"))
 
 bill_with_tip = total_bill*(1+tip/100)
 bill_per_person = round(bill_with_tip/people_count,2)
-print(f"Each person should pay: ${bill_per_person}")
+# print("{:.2f}".format(round(a, 2)))
+print("Each person should pay: ${:.2f}".format(bill_per_person))
